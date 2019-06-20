a27ba521d6ada21dbaf518ad9e31a864f20c808d
Calculuser <jiaqizuo@andrew.cmu.edu>
2019-06-20 16:47:22 -0700

    Automatically enable tracing native stack as long as it is supported by OS.

--- src/config.rs
+++ src/config.rs
@@ -33,11 +33,6 @@ impl Config {
                 .short("F")
                 .long("function")
                 .help("Aggregate samples by function name instead of by line number"))
-            .arg(Arg::with_name("native")
-                .short("n")
-                .long("native")
-                .hidden(!allow_native)
-                .help("Collect stack traces from native extensions written in Cython, C or C++"))
             .arg(Arg::with_name("pid")
                 .short("p")
                 .long("pid")
@@ -117,12 +112,18 @@ impl Config {
         let duration = value_t!(matches, "duration", u64)?;
         let show_line_numbers = matches.occurrences_of("function") == 0;
         let non_blocking = matches.occurrences_of("nonblocking") > 0;
-        let mut native = matches.occurrences_of("native") > 0;
 
-        if !allow_native && native {
-            error!("Native stack traces are not yet supported on this OS. Disabling");
-            native = false;
-        }
+        // Determine whether tracing native stack traces is enabled
+        let native: bool = match allow_native {
+            true => {
+                info!("Native stack traces are supported on this OS. Enabling.");
+                true
+            }
+            false => {
+                info!("Native stack traces are not yet supported on this OS.");
+                false
+            }
+        };
 
         Ok(Config{pid, python_program, dump, flame_file_name, data_file_name,
                   sampling_rate, duration,
